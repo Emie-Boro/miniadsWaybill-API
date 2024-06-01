c91370d1aa304f4eace62c0a32a08d4f0dad59a7
Emie-Trialer <emiedonmukumo@gmail.com>
2024-06-01 13:00:08 +0100

    Added files

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://localhost/miniadsWaybill');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
